8e368c516bce66a04f70276646d4290f2909d899
romanbro <82284311+Roman123-game@users.noreply.github.com>
2025-01-17 15:23:17 +0200

    update 1 file and delete 2 files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
 
-  const arr = new Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) );
-arr.map( ( letter, i ) => { console.log( i, letter ) } );
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Blamk app
     </div>
   );
 }
